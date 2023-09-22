18bdfeed1f23f782b03fb2cca00d3678c93e7a1a
ShinYasuda1 <124539796+ShinYasuda1@users.noreply.github.com>
2023-09-22 11:12:47 +0900

    Delete crud_app directory

--- crud_app/djangoCrudApp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import include, path
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    # CRUD追加
-    path('crud/', include('crud.urls')),
-]
